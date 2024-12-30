5f5a62ca0344c9f793cb79db638685f86eaf7cdc
TarushGupta23 <tarush23.gupta@gmail.com>
2024-12-30 20:36:30 +0530

    added env client ids for google oAuth

--- frontend/src/components/Pawn.jsx
+++ frontend/src/components/Pawn.jsx
@@ -1,20 +1,5 @@
-export function Pawn({index}) {
-    let color = ''
-    switch (Math.floor(index/4)) {
-        case 0:
-            color = 'red'
-            break;
-        case 1:
-            color = 'green'
-            break;
-        case 2:
-            color = 'blue'
-            break;
-        default:
-            color = 'yello'
-            break;
-    }
+export function Pawn({ team }) {
     return (
-        <span className={color}></span>
+        <span className={`color-${team}`}></span>
     )
 }
